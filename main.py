a391a2f103fb647704d053a8fd119f1618ac9938
rohanrudra55 <rohanrudra55@gmail.com>
2024-06-05 14:18:08 +0530

    Updated

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from src import menu
 
 if __name__ == "__main__":
+    os.system('pg_ctl -D database stop')
     os.system('pg_ctl -D database -l logfile.log start')
     menu.manager_system()
     os.system('clear')
     os.system('pg_ctl -D database stop')
+
